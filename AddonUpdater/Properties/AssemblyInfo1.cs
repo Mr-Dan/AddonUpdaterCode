5dcb619fd41b8b99af4d2e30815eff89eaee2cea
Mr-Dan <lopji368@gmail.com>
2023-11-25 22:17:03 +0300

    up

---
+++ AddonUpdater/Properties/AssemblyInfo1.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором 
+// набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("AddonUpdater")]
+[assembly: AssemblyDescription("https://github.com/Mr-Dan/")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AddonUpdater")]
+[assembly: AssemblyCopyright("Copyright © Mr-Dan 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка параметра ComVisible в значение false делает типы в этой сборке невидимыми 
+// для компонентов модели COM.  Если тип в этой сборке необходимо сделать доступным 
+// из модели COM, установите атрибут ComVisible для этого типа в значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("76adacb9-52b2-49a0-a7b7-b3c02eb323e8")]
+
+// Сведения о версии для сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.33")]
+[assembly: AssemblyFileVersion("1.33")]
